313083fbda3d1b2f95c9e8ad979aa426dc25c2a1
Herna Michel <labellehernamichel@yahoo.fr>
2019-04-13 17:20:44 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -30,6 +30,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
      if holiday == :winter
        
      holiday_hash[:winter].each {|k, v| holiday_hash[:winter][k] << supply }
+     holiday_hash[:winter].each {|k, v| holiday_hash[:winter][k] << supply }
    end
      
 
