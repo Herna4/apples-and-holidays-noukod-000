0e723f5f97d06ac9ae7537a4e6d399ab42f80c0e
Herna Michel <labellehernamichel@yahoo.fr>
2019-04-13 18:09:10 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -54,7 +54,9 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-  
+   holiday_hash[:winter].each do |supply|
+     
+   end
    holiday_hash[:winter][:Christmas][:new_years]
 
 end
